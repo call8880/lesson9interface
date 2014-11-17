6c1a8c31a3858ff1d494ecf730f4dc1a9f70bf47
call8880 <barkaimenu@gmail.com>
2014-11-17 08:57:27 -0500

    hmm

---
+++ src/circle.java
@@ -0,0 +1,68 @@
+
+
+import TurtleGraphics.Pen;
+
+public class circle implements shape{
+
+	
+	private double xPos, yPos, radius;
+	
+	public circle(){
+		xPos = 0;
+		yPos = 0;
+		radius = 1;
+	}
+	public circle(double x, double y, double r){
+		xPos = x;
+		yPos = y;
+		radius = r;
+	}
+	
+
+	public double area() {
+		return Math.PI * radius * radius;
+	}
+
+	public void draw(Pen p) {
+		double side = 2.0 * Math.PI * radius/ 120.0;
+		p.up();
+		p.move(xPos + radius, yPos - side / 2.0);
+		p.setDirection(90);
+		p.down();
+		
+		for (int x = 0; x < 120; x++){
+			p.move(side);
+			p.turn(3);
+		}
+		
+	}
+
+	public double getXPos() {
+		return xPos;
+	}
+
+
+	public double getYPos() {
+		return yPos;
+	}
+
+
+	public void move(double xloc, double yloc) {
+		xPos = xloc;
+		yPos = yloc;
+		
+	}
+
+	public void strechBy(double factor) {
+		radius *= factor;
+		
+	}
+	
+	public String toString(){
+		String str = "This is a circle\n";
+		str += "xloc: " + xPos + "\nyloc: " + yPos + "\n";
+		str += "Area: " + area();
+		str += "\nRadius: " + radius;
+		return str;
+	}
+}
---
+++ src/shapeMaker.java
@@ -0,0 +1,51 @@
+
+
+import TurtleGraphics.*;
+
+import java.awt.*;
+
+import TerminalIO.*;
+
+public class shapeMaker {
+	
+	public static void main(String args[]){
+		Pen p = new StandardPen();
+		shape s;
+		double r, x, y, w, h;//xpos, ypos, radius, height, and width
+		int choice;
+		KeyboardReader read = new KeyboardReader();
+		
+		//choose shape
+		while(true){
+			choice = read.readInt("Enter a 1 for a rectangle or a 2 for a circle: ");
+			if (choice == 1 || choice == 2) break;
+		}
+		if (choice == 1){
+			w = read.readDouble("Enter the width of the rectangle: ");
+			h = read.readDouble("Enter the height of the rectanglt: ");
+			s = new rect(0, 0, w, h);
+		}
+		else {
+			r = read.readDouble("Enter the radius of the circle: ");
+			s = new circle(0,0,r);
+		}
+		while(true){
+			s.draw(p);
+			x = read.readDouble("Enter the x position of the shape: ");
+			y = read.readDouble("Enter the y position of the shape: ");
+			//move the pen to the x,y position
+		p.setColor(Color.white);
+		s.draw(p);
+		s.move(x, x);
+		//draw the shape at x,y
+		p.setColor(Color.GREEN);
+		s.draw(p);
+		System.out.println("Shape Drawn");
+		}
+		
+	
+	}
+	
+
+
+}
